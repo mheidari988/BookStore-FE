44818a94218408267472b31ecf3c80124e507146
Reza Heidari <m.heidari988@gmail.com>
2023-11-06 09:47:35 +0300

    add required modules

--- src/app/author/author.module.ts
+++ src/app/author/author.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
 import { AuthorRoutingModule } from './author-routing.module';
 import { AuthorComponent } from './author.component';
-
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AuthorComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthorRoutingModule
-  ]
+  declarations: [AuthorComponent],
+  imports: [SharedModule, AuthorRoutingModule, NgbDatepickerModule],
 })
-export class AuthorModule { }
+export class AuthorModule {}
